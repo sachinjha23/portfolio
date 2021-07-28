72da144f76a3111fa1cc4217516e230de41cc4df
Sachin jha <88023210+sachinjha23@users.noreply.github.com>
2021-07-28 18:57:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 var homes = document.getElementById("home");
 var works = document.getElementById("work");
 var resumes = document.getElementById("resume");
-var resumeBtns = document.getElementById("resumebtn");
 var item1 = document.getElementById("item1");
 var item2 = document.getElementById("item2");
 var item3 = document.getElementById("item3");
@@ -43,18 +42,7 @@ function resume(){
     item3.style.color = "white"
 
 }
-function resumebtn(){
-    homes.style.display = "none"
-    works.style.display = "none"
-    resumes.style.display = "block"
-    works.style.transition = "0s"
-    homes.style.transition = "0s"
-    resumes.style.transition = "0.5s"
-    item1.style.color = "orangered"
-    item2.style.color = "orangered"
-    item3.style.color = "white"
 
-}
 
 
 
